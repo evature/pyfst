16ad935bc27d3ea21bd33cb37f3c4d2c5b9a7301
Ondrej Platek <ondrej.platek@seznam.cz>
2013-10-28 12:18:20 +0100

    Setup.py build does not require Ruby and mustache.
    The mustache functionality was replaced
    by python packages yaml and pystache.
    The extension can be built with one command
    using setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,37 @@ import sys
 import os
 from distutils.core import setup
 from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import yaml
+import pystache
+
+
+templates = [
+    ('fst/_fst.pyx.tpl', 'fst/types.yml', 'fst/_fst.pyx'),
+    ('fst/libfst.pxd.tpl', 'fst/types.yml', 'fst/libfst.pxd'),
+]
+
+
+class pre_build_ext(build_ext):
+    def run(self):
+        '''Before building the C++ extension apply the
+        templates substitution'''
+        print 'running pre_build_ext'
+        try:
+            for templ_name, dic_name, result in templates:
+                with open(dic_name, 'r') as d:
+                    with open(templ_name, 'r') as t:
+                        with open(result, 'w') as r:
+                            dic = yaml.load(d)
+                            tmpl = t.read()
+                            r.write(pystache.render(tmpl, dic))
+                            print 'Created template %s' % result
+            build_ext.run(self)
+        except Exception as e:
+            # how to handle bad cases!
+            print e
+            raise e
+
 
 INC, LIB = [], []
 
@@ -10,13 +41,15 @@ if sys.platform == 'darwin' and os.path.isdir('/opt/local/lib'):
     INC.append('/opt/local/include')
     LIB.append('/opt/local/lib')
 
+
 ext_modules = [
     Extension(name='fst._fst',
-        sources=['fst/_fst.cpp'],
-        libraries=['fst'],
-        extra_compile_args=['-O2'],
-        include_dirs=INC,
-        library_dirs=LIB)
+              sources=['fst/_fst.pyx'],
+              language='c++',
+              include_dirs=INC,
+              libraries=['fst'],
+              library_dirs=LIB,
+              )
 ]
 
 long_description = """
@@ -42,12 +75,13 @@ Example usage::
 
     t[2].final = 3.5
 
-    t.shortest_path() # 2 -(a:A/0.5)-> 1 -(c:C/2.5)-> 0/3.5 
+    t.shortest_path() # 2 -(a:A/0.5)-> 1 -(c:C/2.5)-> 0/3.5
 
 """
 
 setup(
     name='pyfst',
+    cmdclass={'build_ext': pre_build_ext},
     version='0.2.3',
     url='http://pyfst.github.io',
     author='Victor Chahuneau',
